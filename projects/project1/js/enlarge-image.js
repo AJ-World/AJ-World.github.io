3660cab1311d7160c4188d64ba3122931bd8ed34
AJ-World <schacht.ajs@gmail.com>
2026-05-01 14:16:37 -0400

    Add files via upload

---
+++ projects/project1/js/enlarge-image.js
@@ -0,0 +1,20 @@
+<script>
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  const closeBtn = document.querySelector(".lightbox-close");
+
+  document.querySelectorAll(".screenshot-grid img").forEach(img => {
+    img.addEventListener("click", () => {
+      lightbox.style.display = "block";
+      lightboxImg.src = img.src;
+    });
+  });
+
+  closeBtn.onclick = () => {
+    lightbox.style.display = "none";
+  };
+
+  lightbox.onclick = () => {
+    lightbox.style.display = "none";
+  };
+</script>
